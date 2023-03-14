80b7349067cdb54aa2c01b1e62a2ec962c031af4
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-14 15:12:12 +0300

    Completed assignmetn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(e = "roller-skate") {
+  return `This Saturday, I want to ${e}!`;
+}
+//console.log(saturdayFun());
+
+function mondayWork(e = "go to the office") {
+  return `This Monday, I will ${e}.`;
+}
+
+
+
+function wrapAdjective(adjective = "*"){
+    const innerF = function(para = "special") {
+       return `You are ${adjective}${para}${adjective}!`;
+    }
+    return innerF;
+}
+console.log(wrapAdjective());
